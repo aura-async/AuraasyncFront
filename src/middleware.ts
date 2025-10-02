140aaaae734c60a6ea96b94e29e330a59ae466e7
Kanhaiyalal Yadav <kanhaiya13004@gmail.com>
2025-10-02 16:27:44 +0530

    added middleware to fetch affiliate id

---
+++ src/middleware.ts
@@ -0,0 +1,29 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export function middleware(req: NextRequest) {
+  const aff = req.nextUrl.searchParams.get("ref");
+  if (!aff) return NextResponse.next();
+
+  const isProd = process.env.NODE_ENV === "production";
+  const res = NextResponse.next();
+
+  console.log(isProd);
+  res.cookies.set({
+    name: "affiliate_id",
+    value: aff.slice(0, 100),
+    httpOnly: true,
+    sameSite: "lax",
+    secure: isProd,        // false on localhost (HTTP)
+    path: "/",             // visible to all paths
+    // IMPORTANT: no "domain" on localhost
+    // In prod you can do: domain: ".auraasync.in"
+    ...(isProd ? { domain: ".auraasync.in" } : {}),
+    maxAge: 60 * 60 * 24 * 30,
+  });
+
+  return res;
+}
+
+export const config = {
+  matcher: '/',
+};
